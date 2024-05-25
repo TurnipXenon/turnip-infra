25457dfad666840acd4b64709636a8570f0bbf6b
turnipxenon <turnipxenon@gmail.com>
2024-05-25 15:46:23 -0600

    Add Github provider/role for Github Actions

--- lib/turnip-react-infra-stack.ts
+++ lib/turnip-react-infra-stack.ts
@@ -1,12 +1,25 @@
 import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-import {aws_ecr} from "aws-cdk-lib";
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import {aws_ecr, aws_iam} from 'aws-cdk-lib';
+import {Construct} from 'constructs';
+import {ServicePrincipal} from "aws-cdk-lib/aws-iam";
+import {GithubActionsIdentityProvider, GithubActionsRole} from "aws-cdk-github-oidc";
 
 export class TurnipReactInfraStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
 
-    const repository = new aws_ecr.Repository(this, 'turnip-react');
-  }
+        const repository = new aws_ecr.Repository(this, 'turnip-react');
+
+        // References
+        // https://docs.github.com/en/actions/deployment/security-hardening-your-deployments/configuring-openid-connect-in-amazon-web-services#adding-the-identity-provider-to-aws
+        // https://aws.amazon.com/blogs/security/use-iam-roles-to-connect-github-actions-to-actions-in-aws/
+        // https://constructs.dev/packages/aws-cdk-github-oidc/v/2.4.1?lang=typescript
+        const provider = new GithubActionsIdentityProvider(this, "GithubProvider");
+
+        const uploadRole = new GithubActionsRole(this, "CDKGithubActionRole", {
+            provider: provider,
+            owner: "TurnipXenon",
+            repo: "turnip-react-infra",
+        });
+    }
 }
